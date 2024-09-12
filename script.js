fd3f02539234f8ad5903de94ecc3de24135a25c5
Anilkv9883 <72238305+Anilkv9883@users.noreply.github.com>
2024-09-12 10:50:16 +0530

    product slider complete
    product slider section now completed

---
+++ script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".produtSwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    freeMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
