1cedb9c6d3e468ed21e8b288ce75878e832d0a7f
Anilkv9883 <72238305+Anilkv9883@users.noreply.github.com>
2024-09-18 09:51:45 +0530

    products
    all section done  recently added all product

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+// product slider
+
 var swiper = new Swiper(".produtSwiper", {
-    slidesPerView: 5,
+  slidesPerView: 5,
+  spaceBetween: 30,
+  freeMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// product slider 2
+
+var swiper = new Swiper(".produtSwiper2", {
+    slidesPerView: 6,
     spaceBetween: 30,
-    freeMode: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-  });
+});
